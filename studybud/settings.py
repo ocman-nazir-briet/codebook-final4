503590cd1add6191e3bb821831ed8ba8bf73a369
Ocman Nazir Briet <usmannazirbareet@gmail.com>
2023-05-28 17:57:33 +0500

    added new postgresql db

--- studybud/settings.py
+++ studybud/settings.py
@@ -64,10 +64,22 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST':'containers-us-west-199.railway.app',
+        'PORT':'6323',
+        'USER':'postgres',
+        'PASSWORD':'XPgT2VRdeUcBRFJfVPML',
+        'NAME': 'railway',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
