89de066ea78652dc4625e6148cb5e5c3f579f3d6
Ocman Nazir Briet <usmannazirbareet@gmail.com>
2023-06-08 16:46:25 +0500

    added server ip address in setting

--- studybud/settings.py
+++ studybud/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
-ALLOWED_HOSTS = ['codebookpk.herokuapp.com', '127.0.0.1', '*', 'usmannazirbareet.pythonanywhere.com']
+ALLOWED_HOSTS = ['codebookpk.herokuapp.com', '127.0.0.1', '*', 'usmannazirbareet.pythonanywhere.com', '172.31.39.242', '43.207.198.104']
 
 
 INSTALLED_APPS = [
